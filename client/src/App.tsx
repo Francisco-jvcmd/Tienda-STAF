0fe7df23cc60c4fb1dd6985dff669f777fb8f246
Francisco-jvcmd <fcumbajin.finanzas.ladder@gmail.com>
2025-09-02 01:22:37 -0500

    Se corrigió el error de sintaxin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import Home from "@/pages/home";
 import About from "@/pages/about";
-import NotFound from "@/pages/not-found";
+import quienes-somos from "@/pages/quienes-somos";
 
 function Router() {
   return (
